240cf81540a1441a90c364547e71bb92cd31f8e1
Adiba Akter <98072437+AdibaAdi@users.noreply.github.com>
2026-04-10 15:03:48 -0500

    Build civic dashboard with Leaflet map, filters, and upvoting

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,11 +2,11 @@ import Hero from '../components/Hero'
 import Navbar from '../components/Navbar'
 import StatsCounter from '../components/StatsCounter'
 
-function LandingPage() {
+function LandingPage({ onReportClick }) {
   return (
     <main className="min-h-screen bg-civic-night text-white">
       <Navbar />
-      <Hero />
+      <Hero onReportClick={onReportClick} />
       <StatsCounter />
     </main>
   )
